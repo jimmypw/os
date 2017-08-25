48f54bf88ef9d88ddad41facc5790c71f9cd56e4
James Park-Watt <jparkwatt@ukcloud.com>
2017-08-25 14:30:51 +0100

    Refactored the repository filesystem
    Moved data to a sensible location

--- boot.asm
+++ boot.asm
@@ -1,6 +1,25 @@
 [org 0x7c00]
 BITS 16
 
+mov [BOOTDRIVE], bl    ; Store the boot drive set by BIOS
+
+mov       bx,       HELLO_MSG
+call      println
+
+mov       bx,       GOODBYE_MSG
+call      println
+
+mov       dx,       [BOOTDRIVE]
+call      print_hex_16
+
+mov       bx,       HEXOUT
+call      println
+
+jmp       $                     ; Jump to this address forever
+
+%include "string/print_string.asm"
+%include "hex/print_hex.asm"
+
 HELLO_MSG:
   db "HELLO WOLD", 0
 
@@ -10,22 +29,8 @@ GOODBYE_MSG:
 HEXOUT:
   db "0x0000", 0
 
-mov       bx,       HELLO_MSG
-call      println
-
-mov       bx,       GOODBYE_MSG
-call      println
-
-mov       dx,       0xbee1
-call      print_hex_16
-
-mov       bx,       HEXOUT
-call      println
-
-jmp       $                     ; Jump to this address forever
-
-%include "print_string.asm"
-%include "print_hex.asm"
+BOOTDRIVE:
+  db 0
 
 times  510-($-$$) db 0    ; Pad the boot loader to 512 bytes
 dw 0xaa55                 ; Boot sector magic number
